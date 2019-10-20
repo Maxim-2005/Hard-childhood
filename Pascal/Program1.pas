8b628a1ebd584010c89b844d1d88da0b90ce1f97
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:13:06 +0300

    Create Program1.pas

---
+++ Pascal/Program1.pas
@@ -0,0 +1,25 @@
+﻿Program HelloWorld;
+
+Var x,y:Integer;
+A, B, C:Real;
+
+Begin
+
+X := 5;
+A := 3.14;
+B := 2.18;
+Writeln (A*B);
+Writeln ('ВведитеЧисло');
+Readln (Y);
+
+Writeln (x);
+Writeln (y);
+Writeln (x+y);
+
+if (x+y=10) then ('HelloWorld') {
+Writln ('GoodBayWorld')
+};
+
+Write ('HelloWorld');
+
+End.
