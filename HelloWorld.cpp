a8deb28b873126dd3b278c7fa5c13a20dd21b477
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:12:57 +0300

    Delete HelloWorld.cpp

--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> //Стандартная библиотека ввода и вывода
-
-using namespace std; //Пространство имен для библиотеки
-
-void main () //Старт программы
-{
-	cout <<"Hello World"<< endl; //Вывод на экран консоли
-	system ("pause"); //Пауза
-}
