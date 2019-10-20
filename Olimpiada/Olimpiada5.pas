7378ff58e98136553db180411c61a71c6e50575b
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:22:51 +0300

    Create Olimpiada5.pas

---
+++ Olimpiada/Olimpiada5.pas
@@ -0,0 +1,17 @@
+﻿Program Olimpiada5;
+var 
+arr :array[1..20] of integer;
+N, s :integer;
+
+begin
+  
+  Write('Введите число данных от 1 до 20: ');
+  Readln(N);
+  Writeln('Введите ', N, ' данных');
+  for var i := 1 to N do
+    begin
+       Readln(arr[i]);
+       if arr[i] > i then s := s + arr[i];
+    end;
+  Writeln('Сумма привышающих данных: ', s); 
+end.
