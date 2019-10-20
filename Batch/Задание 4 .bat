b5ffed9358ea29e873ef1a4c1d42677c3e063754
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:09:20 +0300

    Create Задание 4 .bat

---
+++ Batch/Задание 4 .bat
@@ -0,0 +1,16 @@
+@echo off
+chcp 1251
+color 0a
+cls
+:1
+set /p S=¬ведите размер треугольника: 
+echo:
+echo 
+for /L %%N in (3,1,%S%) do (
+<nul set /p = 
+for /L %%N in (4,1,%%N) do <nul set /p =. 
+echo  )
+for /L %%N in (1,1,%S%) do <nul set /p = 
+pause >nul
+cls
+goto 1
