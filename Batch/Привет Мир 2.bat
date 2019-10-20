939d2487788b30f034f6ebc26a729a421cd23119
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:09:34 +0300

    Create Привет Мир 2.bat

---
+++ Batch/Привет Мир 2.bat
@@ -0,0 +1,14 @@
+rem Привет Мир
+:: Привет Мир
+@echo off
+chcp 1251
+cls
+title Привет Мир
+mode con cols=80 lines=25
+color 0a
+echo: & echo:
+echo Привет Мир
+date /t
+time /t
+timeout /t 10
+pause
