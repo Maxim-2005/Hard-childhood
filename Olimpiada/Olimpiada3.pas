90a70e0404dcb082189b8a91437bf7158a01e84b
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-20 21:23:00 +0300

    Create Olimpiada3.pas

---
+++ Olimpiada/Olimpiada3.pas
@@ -0,0 +1,11 @@
+﻿Program Olimpiada4;
+var a, b, c, d, s:integer;
+begin
+
+Writeln('Введите числа 4 видов птиц: ');
+Readln(a, b, c, d);
+s := a + b + c + d;
+Write('Всего ', s, ' птиц');
+if ((s mod 10) = 1) and ((s mod 100) <> 11) then Write('а');
+if ((s mod 10) > 1) and ((s mod 10) < 5) and (((s mod 100) < 11) or ((s mod 100) > 15)) then Write('ы');
+end.
