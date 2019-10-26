770b04d95c6f29f4e10cc2316ea6783cdd10ff6c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-26 20:53:14 +0300

    Update Tetris.java

--- Java/Tetris/Tetris.java
+++ Java/Tetris/Tetris.java
@@ -1,5 +1,17 @@
+import javax.swing.JFrame;
+
 public class Tetris {
+	
+	protected static int block = 50;
+	
 	public static void main(String[] args){
-		System.out.println("HelloWord");
+		JFrame jFrame = new JFrame("Tetris");
+		JFrame.setDefaultLookAndFeelDecoration(true);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.pack();
+		jFrame.setSize(block*10, block*20);
+		jFrame.setResizable(false);
+		jFrame.setLocationRelativeTo(null);	
+		jFrame.setVisible(true);
 	}
 }
