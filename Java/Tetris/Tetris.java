dffaa76777413c928b896892b17e8adb0a27e760
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2019-10-27 21:31:30 +0300

    Update Tetris.java

--- Java/Tetris/Tetris.java
+++ Java/Tetris/Tetris.java
@@ -1,17 +1,51 @@
-import javax.swing.JFrame;
+/*
+ * Tetris
+ * Maxim 27.10.2019
+ */
 
-public class Tetris {
-	
-	protected static int block = 50;
-	
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tetris extends JPanel {
+
+	//Variables
+	protected static int block = 50, x = 10;
+
 	public static void main(String[] args){
+		
+		//Application window
 		JFrame jFrame = new JFrame("Tetris");
-		JFrame.setDefaultLookAndFeelDecoration(true);
+		JFrame.setDefaultLookAndFeelDecorated(true);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.pack();
 		jFrame.setSize(block*10, block*20);
 		jFrame.setResizable(false);
 		jFrame.setLocationRelativeTo(null);	
 		jFrame.setVisible(true);
+		Tetris tetris = new Tetris();
+		jFrame.add(tetris);
+	
+		while(true) {
+			tetris.game();
+			tetris.repaint();
+			try {
+				Thread.sleep(125);
+			} catch (Exception e) {}
+		}
+	}
+
+	//Logic block
+	private void game() {
+		x++;
+	}
+	
+	//Drawing graphics
+	public void paint(Graphics ctx) {
+		super.paint(ctx);
+		setBackground(Color.black);
+		
+		ctx.setColor(Color.white);
+		ctx.fillRect(50, x, 50, 50);
 	}
 }
