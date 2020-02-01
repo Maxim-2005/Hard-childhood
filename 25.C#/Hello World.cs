1966c4f17ed2ea5fdfca074fe439c06257782db7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-01 21:03:43 +0300

    Create Hello World.cs

---
+++ 25.C#/Hello World.cs
@@ -0,0 +1,20 @@
+﻿ /*
+ *Уроки по C#
+ *Максим Покидько Серегеевич
+ *01.02.2020
+ */
+
+using System;
+
+namespace Myprogramm
+{
+	class HW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.WriteLine(DateTime.Now);
+			Console.ReadKey();
+		}
+	}
+}
