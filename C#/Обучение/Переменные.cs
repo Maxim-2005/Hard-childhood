2fc98e575312d55adacf9dd1d18d2d30761ff0aa
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-05 17:02:52 +0300

    Create Переменные.cs

---
+++ C#/Обучение/Переменные.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Обучение
+{
+    class Переменные
+    {
+        public void Консоль()
+        {
+            Console.Write("Сравнение 2 больше 3 - ");
+            Console.WriteLine(2 < 3);
+            Console.WriteLine();
+
+            Console.Write("Сравнение 2 больше 3 - ");
+            bool x = 2 > 3;
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+            Console.Write("Сравнение 2 больше 3 - ");
+            x = 2 == 3;
+            Console.WriteLine(x);
+            Console.WriteLine();
+        }
+    }
+}
