0ded1ee609f97479ebc09610e7bb1bf1de3d0824
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-08 16:36:53 +0300

    Update Переменные.cs

--- C#/Обучение/Переменные.cs
+++ C#/Обучение/Переменные.cs
@@ -10,15 +10,11 @@ namespace Обучение
             Console.WriteLine(2 < 3);
             Console.WriteLine();
 
-            Console.Write("Сравнение 2 больше 3 - ");
             bool x = 2 > 3;
-            Console.WriteLine(x);
-            Console.WriteLine();
+            Console.WriteLine($"Сравнение 2 больше 3 - {x}\n");
 
-            Console.Write("Сравнение 2 больше 3 - ");
             x = 2 == 3;
-            Console.WriteLine(x);
-            Console.WriteLine();
+            Console.Write($"Сравнение 2 больше 3 - {x}\n");
         }
     }
 }
