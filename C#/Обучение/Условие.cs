fb79f4894060c3d083c8b920ef88b3850f7f7bb0
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-08 16:36:51 +0300

    Create Условие.cs

---
+++ C#/Обучение/Условие.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Обучение2
+{
+    class Условие
+    {
+        public void Console2()
+        {
+            bool x = 2 < 3;
+            if (x)
+            {
+                Console.Write($"Условие верно\n");
+            }
+            else
+            {
+                Console.WriteLine("Условие не верно");
+            }
+        }
+    }
+}
