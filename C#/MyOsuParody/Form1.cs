2b696a5441c50f4c8d51dc43412b673bd1211c5c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-16 20:12:27 +0300

    Update Form1.cs

--- C#/MyOsuParody/Form1.cs
+++ C#/MyOsuParody/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,18 +13,53 @@ namespace MyOsuParody
 {
     public partial class Form1 : Form
     {
-        private Bitmap target = Resource1.circle;
+        //Variable's and object's
+        private Random random = new Random();
+        private Bitmap target = Resource1.cursor;
+        private Point point = Point.Empty;
+        //private Bitmap circle = Resource1.circle;
+
+        //Star window
         public Form1()
         {
             InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+
         }
 
+        //Paint window
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics ctx = e.Graphics;
+            ctx.SmoothingMode = SmoothingMode.AntiAlias;
+            int score = 0;
+            score++;
+            label1.Text = score.ToString();
 
-            ctx.DrawImage(target, 100, 100, 100, 100);
-            ctx.DrawEllipse(new Pen(Color.Black, 2), 200, 200, 100, 100);
+            var position = this.PointToClient(Cursor.Position);
+
+            point.X = 400 + random.Next(-4, 4) * 100;
+            point.Y = 250 + random.Next(-3, 3) * 100;
+
+            //ctx.DrawImage(circle, 100, 100, 100, 100); ;
+            ctx.DrawEllipse(new Pen(Color.Black, 2), point.X, point.Y, 100, 100);
+            ctx.DrawImage(target, position.X - 75, position.Y - 75, 150, 150);
+        }
+
+        //Update window
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
