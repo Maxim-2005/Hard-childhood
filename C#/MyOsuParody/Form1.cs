df3eabdd52490575764c9b71d260f41b034ce264
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-23 13:53:27 +0300

    Update Form1.cs

--- C#/MyOsuParody/Form1.cs
+++ C#/MyOsuParody/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace MyOsuParody
@@ -17,12 +12,17 @@ namespace MyOsuParody
         private Random random = new Random();
         private Bitmap target = Resource1.cursor;
         private Point point = Point.Empty;
+        private Pen pen = new Pen(Color.Black, 2);
+        private SoundPlayer soundPlayer = new SoundPlayer(Resource1.hit);
         private int score;
         //private Bitmap circle = Resource1.circle;
 
         //Star window
         public Form1()
         {
+            //Hide cursor
+            Cursor.Hide();
+
             InitializeComponent();
 
             SetStyle(ControlStyles.OptimizedDoubleBuffer |
@@ -39,39 +39,50 @@ namespace MyOsuParody
         {
             Graphics ctx = e.Graphics;
             ctx.SmoothingMode = SmoothingMode.AntiAlias;
-            label1.Text = score.ToString();
 
             Point position = this.PointToClient(Cursor.Position);
-
             //ctx.DrawImage(circle, 100, 100, 100, 100); ;
-            Rectangle cursorPosition = new Rectangle(position.X - 75, position.Y - 75, 150, 150);
+            Rectangle cursorPosition = new Rectangle(position.X - 50, position.Y - 50, 100, 100);
             Rectangle targetPosition = new Rectangle(point.X, point.Y, 100, 100);
 
-            ctx.DrawEllipse(new Pen(Color.Black, 2), targetPosition);
+            ctx.DrawEllipse(pen, targetPosition);
             ctx.DrawImage(target, cursorPosition);
+
+            int katetX =cursorPosition.X - targetPosition.X;
+            int katetY =cursorPosition.Y - targetPosition.Y;
+            int Hipotinuza = (int) Math.Sqrt(katetX*katetX + katetY*katetY);
+
+            label1.Text = Hipotinuza.ToString();
         }
 
         //Update window
         private void timer1_Tick(object sender, EventArgs e)
         {
             Refresh();
-            score++;
         }
 
         //Move circle
         private void RandomTarget()
         {
-            point.X = 400 + random.Next(-4, 4) * 100;
-            point.Y = 250 + random.Next(-3, 3) * 100;
+            point.X = Width/2 + random.Next(-4, 4) * 100;
+            point.Y = Height/2 + random.Next(-3, 3) * 100;
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            RandomTarget();
+            StepGame();
         }
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            StepGame();
+        }
+
+        //Step game
+        private void StepGame()
+        {
+            score++;
+            soundPlayer.Play();
             RandomTarget();
         }
     }
