708933e3888bb06304e467259b28cf615a97013f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-02-29 17:02:30 +0300

    Update Условие.cs

--- C#/Обучение/Условие.cs
+++ C#/Обучение/Условие.cs
@@ -6,15 +6,39 @@ namespace Обучение2
     {
         public void Console2()
         {
-            bool x = 2 < 3;
+
+            bool a = true & true; //И
+            bool b = false ^ false; //Либо
+            bool x = a | b; // Или
             if (x)
             {
-                Console.Write($"Условие верно\n");
+                Console.Write("Условие верно");
             }
             else
             {
                 Console.WriteLine("Условие не верно");
             }
+
+            Console.WriteLine($"Сравнение + и + - {true & true}\n");
+            Console.WriteLine($"Сравнение + и + - {false & false}\n");
+            Console.WriteLine($"Сравнение + и + - {false & true}\n");
+
+            Console.WriteLine($"Сравнение + или + - {true | true}\n");
+            Console.WriteLine($"Сравнение + или + - {false | false}\n");
+            Console.WriteLine($"Сравнение + или + - {false | true}\n");
+
+            Console.WriteLine($"Сравнение + и + - {true ^ true}\n");
+            Console.WriteLine($"Сравнение + и + - {false ^ false}\n");
+            Console.WriteLine($"Сравнение + и + - {false ^ true}\n");
+
+            int i = 2147400000;
+            bool y = true;
+            do
+            {
+                //y = i < 10;
+                Console.WriteLine(i++);
+            }
+            while (y);
         }
     }
 }
