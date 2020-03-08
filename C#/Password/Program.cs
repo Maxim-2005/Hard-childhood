985184f5af58e008577e35ac7bd2ecf1d457742c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-03-08 15:48:10 +0300

    Create Program.cs

---
+++ C#/Password/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменные
+            string password = "1234", _password = "";
+
+            //Проверка пароля
+            while (password != _password)
+            {
+                Console.Write("Введите пароль: ");
+                _password = Console.ReadLine();
+                if (password != _password) Console.WriteLine("Доступ запрещен");
+            }
+
+            //Доступ
+            Console.WriteLine("Доступ разрешен");
+            Console.ReadKey();
+        }
+    }
+}
