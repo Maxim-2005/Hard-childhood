ee696c026a4260be39daff03522c03a4f6593179
Maxim-2005 <KoT120461>
2020-03-23 21:08:49 +0300

    Create Form1.cs

---
+++ C#/Fraktal/Fraktal/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fraktal
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+        private Point point;
+        private Point[] angle;
+        private Random random;
+        private int number, rand;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
