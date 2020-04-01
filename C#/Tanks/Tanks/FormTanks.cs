71267541b9fe82b712c30ee9dccf30cd64dfc26b
Maxim-2005 <KoT120461>
2020-04-01 21:21:10 +0300

    Update FormTanks.cs

--- C#/Tanks/Tanks/FormTanks.cs
+++ C#/Tanks/Tanks/FormTanks.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tanks
@@ -13,8 +7,7 @@ namespace Tanks
     public partial class FormTanks : Form
     {
         Graphics g;
-        Tank tank = new Tank();
-        Random random = new Random();
+        ListTanks listTanks;
 
         public FormTanks()
         {
@@ -39,23 +32,18 @@ namespace Tanks
                 timer.Enabled = true;
             else timer.Enabled = false;
         }
-        
-
 
         private void FormTanks_Paint(object sender, PaintEventArgs e)
         {
             x++;
             label1.Text = x.ToString();
             g = e.Graphics;
+            listTanks.DrawListTank(g);
+        }
 
-            Point position = Point.Empty;
-
-            //for (byte i = 0; i < 10; i++)
-            //{
-                position.X = 500;
-                position.Y = 300;
-                tank.DrawTank(g, position);
-            //}
+        private void FormTanks_Load(object sender, EventArgs e)
+        {
+            listTanks = new ListTanks();
         }
     }
 }
