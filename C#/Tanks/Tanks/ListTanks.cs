339fa668e0d84de0941dfd12ed79ab0d54b13bd3
Maxim-2005 <KoT120461>
2020-04-01 21:21:17 +0300

    Create ListTanks.cs

---
+++ C#/Tanks/Tanks/ListTanks.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class ListTanks
+    {
+        Tank tank;
+
+        public void DrawListTank(Graphics g)
+        {
+            for (byte i = 0; i < 10; i++)
+            {
+                tank = new Tank();
+                tank.DrawTank(g);
+            }
+        }
+    }
+}
