9e826241fa5fe51433c12582b62bfedd4d588e0c
Maxim-2005 <KoT120461>
2020-04-02 11:15:51 +0300

    Update FormTanks.cs

--- C#/Tanks/Tanks/FormTanks.cs
+++ C#/Tanks/Tanks/FormTanks.cs
@@ -9,6 +9,7 @@ namespace Tanks
         Graphics g;
         ListTanks listTanks;
 
+        //Окно приложения
         public FormTanks()
         {
             InitializeComponent();
@@ -20,30 +21,32 @@ namespace Tanks
             UpdateStyles();
         }
 
-        int x;
+        //Загрузка окна
+        private void FormTanks_Load(object sender, EventArgs e)
+        {
+            listTanks = new ListTanks();
+            listTanks.CreateListTanks();
+        }
+
+        //Обновление окна
+        private void FormTanks_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            listTanks.DrawListTank(g);
+        }
+
+        //Таймер
         private void timer1_Tick(object sender, EventArgs e)
         {
             Refresh();
         }
 
+        //Старт || Стоп
         private void FormTanks_Click(object sender, EventArgs e)
         {
             if (timer.Enabled == false)
                 timer.Enabled = true;
             else timer.Enabled = false;
-        }
-
-        private void FormTanks_Paint(object sender, PaintEventArgs e)
-        {
-            x++;
-            label1.Text = x.ToString();
-            g = e.Graphics;
-            listTanks.DrawListTank(g);
-        }
-
-        private void FormTanks_Load(object sender, EventArgs e)
-        {
-            listTanks = new ListTanks();
         }
     }
 }
