d17ce9feb3d74bb616ea34614ddd4a72f863b1e6
Maxim-2005 <KoT120461>
2020-04-03 20:12:16 +0300

    Update ListTanks.cs

--- C#/Tanks/Tanks/ListTanks.cs
+++ C#/Tanks/Tanks/ListTanks.cs
@@ -1,33 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Tanks
 {
     class ListTanks : List<Tank>
     {
-        public Tank tank = new Tank();
-        public List<Tank> listTanks = new List<Tank>();
+        public byte count = 10;
+
+        private List<Tank> listTanks = new List<Tank>();
+        private Random random = new Random();
 
         //Создание списка танков
         public List<Tank> CreateListTanks()
         {
-            for (byte i = 1; i <= 10; i++)
+            for (byte i = 1; i <= count; i++)
             {
                 listTanks.Add(new Tank()
                 {
                     id = i,
-                    position = tank.Position()
+                    position = StartPosition()
                 });
             }
             return listTanks;
         }
 
+        //Стартовая позиция
+        public Point StartPosition()
+        {
+            Point position = new Point();
+            position.X = random.Next(1280);
+            position.Y = random.Next(720);
+            return position;
+        }
+
         //Отрисовка списка танков
-        public void DrawListTank(Graphics g)
+        public void DrawListTank(Graphics g, Point cursor)
         {
             foreach (Tank tank in listTanks)
             {
-                tank.DrawTank(g);
+                tank.DrawTank(g, cursor);
             }
         }
     }
