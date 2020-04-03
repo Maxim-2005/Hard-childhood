e89387d16a458ea77201c3a30472cc4e6ea71911
Maxim-2005 <KoT120461>
2020-04-03 20:12:14 +0300

    Update Tank.cs

--- C#/Tanks/Tanks/Tank.cs
+++ C#/Tanks/Tanks/Tank.cs
@@ -5,21 +5,25 @@ namespace Tanks
 {
     class Tank
     {
-        Bitmap bitmap = new Bitmap(Properties.Resources.unnamed);
-        Rectangle body = new Rectangle(new Point(0, 0), new Size(128, 128));
-        Rectangle tower = new Rectangle(new Point(128, 0), new Size(128, 128));
-
-        //Танк
         public int id; //Номер танка
-        public Point position; //Позиция на карте
-        public Point target; //Цель танка
-        public float vector; //Угол повората корпуса
-        public float vectorTower; //Угол поворота башни
-        public Random random = new Random();
+        public PointF position; //Позиция на карте
+
+        private Bitmap bitmap = new Bitmap(Properties.Resources.unnamed);
+        private Rectangle body = new Rectangle(new Point(0, 0), new Size(128, 128));
+        private Rectangle tower = new Rectangle(new Point(128, 0), new Size(128, 128));
+        private PointF target; //Цель танка
+        private float vector; //Угол повората корпуса
+        private float vectorTower; //Угол поворота башни
+        private float speed = 1; //Скорость танка
 
         //Отрисовка танка
-        public void DrawTank(Graphics g)
+        public void DrawTank(Graphics g, Point cursor)
         {
+            target = cursor;
+            Position();
+            Vector();
+            vectorTower = vector+15;
+
             //Корпус
             g.TranslateTransform(position.X, position.Y);
             g.RotateTransform(vector);
@@ -29,15 +33,25 @@ namespace Tanks
             //Башня
             g.TranslateTransform(position.X, position.Y);
             g.RotateTransform(vectorTower);
-            g.DrawImage(bitmap, -64, -60, tower, GraphicsUnit.Pixel);
+            g.DrawImage(bitmap, -68, -76, tower, GraphicsUnit.Pixel);
             g.ResetTransform();
         }
 
-        //Расчет позиции танка
-        public Point Position()
+    //Расчет поворота танка
+    private float Vector()
         {
-            position.X = random.Next(1280);
-            position.Y = random.Next(720);
+            float katetX = target.X - position.X;
+            float katetY = target.Y - position.Y;
+            vector = (float)(Math.Atan2(katetY, katetX) * 180 / Math.PI + 90);
+            
+            return vector;
+        }
+
+    //Расчет позиции танка
+    private PointF Position()
+        {
+            position.X += speed*(float)Math.Cos(vector);
+            position.Y += speed*(float)Math.Sin(vector);
             return position;
         }
     }
