97fe741e4eecebd44efc1edd62b963df51fb2a1e
Maxim-2005 <KoT120461>
2020-04-12 12:04:57 +0300

    Create Unit.cs

---
+++ C#/Tanks/Tanks/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tanks
+{
+    abstract class Unit
+    {
+        public int id; //Номер танка
+        public PointF position; //Позиция на карте
+        public PointF target; //Цель танка
+        public float vector; //Угол повората корпуса
+        public float speed = 1; //Скорость танка
+
+        //Расчет поворота танка
+        public float Vector()
+        {
+            float katetX = target.X - position.X;
+            float katetY = target.Y - position.Y;
+            vector = (float)(Math.Atan2(katetY, katetX) * 180 / Math.PI + 90);
+
+            return vector;
+        }
+
+        //Расчет позиции танка
+        public PointF Position()
+        {
+            position.X += speed * (float)Math.Cos(vector);
+            position.Y += speed * (float)Math.Sin(vector);
+            return position;
+        }
+    }
+}
