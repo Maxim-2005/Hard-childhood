b0fb24b34cf65668764e330b86f2e8191dcb1d00
Maxim-2005 <KoT120461>
2020-04-12 12:05:12 +0300

    Create Car.cs

---
+++ C#/Tanks/Tanks/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Car : Unit
+    {
+        private Bitmap bitmap = new Bitmap(Properties.Resources.Машинка);
+        private Rectangle body = new Rectangle(new Point(0, 0), new Size(64, 64));
+
+        //Отрисовка машинки
+        public void DrawCar(Graphics g, Point cursor)
+        {
+            target = cursor;
+            Position();
+            Vector();
+
+            //Машинка
+            g.TranslateTransform(position.X, position.Y);
+            g.RotateTransform(vector);
+            g.DrawImage(bitmap, -62, -59, body, GraphicsUnit.Pixel);
+            g.ResetTransform();
+        }
+    }
+}
