898f18cbde4e71e46a2c4329c7d8726e2df5df37
Maxim-2005 <KoT120461>
2020-04-12 12:05:07 +0300

    Update Resources.Designer.cs

--- C#/Tanks/Tanks/Properties/Resources.Designer.cs
+++ C#/Tanks/Tanks/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Tanks.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Машинка {
+            get {
+                object obj = ResourceManager.GetObject("Машинка", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
