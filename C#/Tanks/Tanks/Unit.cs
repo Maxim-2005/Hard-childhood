1cabd0a84b9b745efc26efcc35f343ef64f7a2aa
Maxim-2005 <KoT120461>
2020-04-20 15:10:06 +0300

    Update Unit.cs

--- C#/Tanks/Tanks/Unit.cs
+++ C#/Tanks/Tanks/Unit.cs
@@ -5,11 +5,29 @@ namespace Tanks
 {
     abstract class Unit
     {
-        public int id; //Номер танка
+        private static uint ID;
+
+        public uint id = ++ID; //Номер танка
         public PointF position; //Позиция на карте
         public PointF target; //Цель танка
         public float vector; //Угол повората корпуса
         public float speed = 1; //Скорость танка
+        private Font font = new Font("Arial", 10, FontStyle.Bold, GraphicsUnit.Point);
+        private SolidBrush color = new SolidBrush(Color.Yellow);
+        private Pen pen = new Pen(Color.Red, 3);
+
+        //Номер и полоска жизни
+        public void DrawInfo(Graphics g)
+        {
+            //Наименование
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawString(id.ToString(), font, color, -10, -50);
+            g.ResetTransform();
+            //Жизнь
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawLine(pen, -30, -30,  30, -30);
+            g.ResetTransform();
+        }
 
         //Расчет поворота танка
         public float Vector()
