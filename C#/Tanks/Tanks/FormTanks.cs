664881a13ac8b0c8ad3492ac9554e1d2ce4adb8d
Maxim-2005 <KoT120461>
2020-05-01 12:07:34 +0300

    Update FormTanks.cs

--- C#/Tanks/Tanks/FormTanks.cs
+++ C#/Tanks/Tanks/FormTanks.cs
@@ -27,6 +27,9 @@ namespace Tanks
         {
             listUnit = new ListUnit();
             listUnit.CreateListUnit();
+
+            //SW();
+            Sound();
         }
         
         //Обновление окна
@@ -49,6 +52,81 @@ namespace Tanks
             if (timer.Enabled == false)
                 timer.Enabled = true;
             else timer.Enabled = false;
+        }
+
+        //Звук заставки
+        private void SW()
+                    //261 - до 1
+        //293 - ре 2
+        //329 - ми 3
+        //349 - фа 4
+        //392 - соль 5
+        //440 - ля 6
+        //493 - си 7
+        {
+            Console.Beep(440, 500);
+            Console.Beep(440, 500);
+            Console.Beep(440, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 1000);
+            Console.Beep(659, 500);
+            Console.Beep(659, 500);
+            Console.Beep(659, 500);
+            Console.Beep(698, 350);
+            Console.Beep(523, 150);
+            Console.Beep(415, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 1000);
+        }
+        private void Sound()
+        {
+            Console.Beep(349, 250);
+            Console.Beep(330, 250);
+            Console.Beep(293, 250);
+            Console.Beep(261, 250);
+            Console.Beep(392, 500);
+            Console.Beep(392, 350);
+            Console.Beep(349, 250);
+            Console.Beep(329, 250);
+            Console.Beep(293, 250);
+            Console.Beep(261, 250);
+            Console.Beep(392, 500);
+            Console.Beep(392, 350);
+
+            Console.Beep(349, 250);
+            Console.Beep(440, 250);
+            Console.Beep(440, 250);
+            Console.Beep(349, 250);
+            Console.Beep(329, 250);
+            Console.Beep(392, 250);
+            Console.Beep(392, 250);
+            Console.Beep(329, 250);
+            Console.Beep(293, 250);
+            Console.Beep(329, 250);
+            Console.Beep(349, 250);
+            Console.Beep(293, 250);
+            Console.Beep(261, 500);
+            Console.Beep(261, 500);
+
+            Console.Beep(349, 250);
+            Console.Beep(440, 250);
+            Console.Beep(440, 250);
+            Console.Beep(349, 250);
+            Console.Beep(329, 250);
+            Console.Beep(392, 250);
+            Console.Beep(392, 250);
+            Console.Beep(329, 250);
+            Console.Beep(293, 250);
+            Console.Beep(329, 250);
+            Console.Beep(349, 250);
+            Console.Beep(293, 250);
+            Console.Beep(261, 500);
+            Console.Beep(261, 500);
         }
     }
 }
