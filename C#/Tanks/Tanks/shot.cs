59e0bd9f927959c184453f1e21c47dba10f9b528
Maxim-2005 <KoT120461>
2020-05-04 21:21:31 +0300

    Create shot.cs

---
+++ C#/Tanks/Tanks/shot.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Shot : AObject
+    {
+        Pen pen;
+
+        //Отрисовка пульки
+        public void DrawShot(Graphics g, Point cursor)
+        {
+            target = cursor;
+            pen = new Pen(Color.DarkOrange, 3);
+            g.DrawLine(pen, position, target);
+        }
+    }
+}
