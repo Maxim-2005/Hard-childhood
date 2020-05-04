f474b33417967679d4d4307b1f8f0d9cd31362a9
Maxim-2005 <KoT120461>
2020-05-04 21:21:45 +0300

    Create AObject.cs

---
+++ C#/Tanks/Tanks/AObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    abstract class AObject
+    {
+        public PointF position; //Позиция на карте
+        public PointF target; //Цель танка
+        public float speed; //Скорость танка
+    }
+}
