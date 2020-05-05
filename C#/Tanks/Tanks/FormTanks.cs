1992727278ff3ab45784807a0f875b89e68c1f70
Maxim-2005 <KoT120461>
2020-05-05 21:11:02 +0300

    Update FormTanks.cs

--- C#/Tanks/Tanks/FormTanks.cs
+++ C#/Tanks/Tanks/FormTanks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,7 +11,7 @@ namespace Tanks
         public Graphics g;
         private ListUnit listUnit;
         private Point cursor;
-        Shot shot = new Shot();
+        Shot shot;
 
         //Окно приложения
         public FormTanks()
@@ -32,17 +33,22 @@ namespace Tanks
 
             //SW();
             Sound();
+
+            shot = new Shot();
+            shot.position = new PointF(50, 50);
+            shot.target = new PointF(1000, 600);
+            shot.speed = 20;
         }
         
         //Обновление окна
         private void FormTanks_Paint(object sender, PaintEventArgs e)
         {
             g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
             cursor = PointToClient(Cursor.Position);
             listUnit.DrawListUnit(g, cursor);
 
-            shot.position = new PointF(50, 50);
-            shot.DrawShot(g, cursor);
+            shot.DrawShot(g);
         }
 
         //Таймер
