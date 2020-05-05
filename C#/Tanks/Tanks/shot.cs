544205450871e9245530a77f604c3b7a60e046ca
Maxim-2005 <KoT120461>
2020-05-05 21:11:07 +0300

    Update shot.cs

--- C#/Tanks/Tanks/shot.cs
+++ C#/Tanks/Tanks/shot.cs
@@ -1,17 +1,41 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Tanks
 {
     class Shot : AObject
     {
+        private PointF position0;
         Pen pen;
 
         //Отрисовка пульки
-        public void DrawShot(Graphics g, Point cursor)
+        public void DrawShot(Graphics g)
         {
-            target = cursor;
+            position = Position();
             pen = new Pen(Color.DarkOrange, 3);
-            g.DrawLine(pen, position, target);
+            g.DrawLine(pen, position, position0);
+        }
+
+        //Расчет позиции пульки
+        public PointF Position()
+        {
+            vector = Vector();
+            position0 = position;
+            position.X += speed * (float)Math.Cos(vector);
+            position.Y += speed * (float)Math.Sin(vector);
+
+            return position;
+        }
+
+        //Угол на цель
+        private float Vector()
+        {
+            float katetX = target.X - position.X;
+            float katetY = target.Y - position.Y;
+            vector = (float)(Math.Atan2(katetY, katetX) * 180 / Math.PI + 90);
+            if (vector < 0) vector += 360;
+
+            return vector;
         }
     }
 }
