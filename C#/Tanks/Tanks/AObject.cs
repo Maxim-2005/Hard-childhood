9aa2ce9675210b3e39e5fc3cee874f43cbfb4729
Maxim-2005 <KoT120461>
2020-05-06 21:37:57 +0300

    Update AObject.cs

--- C#/Tanks/Tanks/AObject.cs
+++ C#/Tanks/Tanks/AObject.cs
@@ -4,6 +4,7 @@ namespace Tanks
 {
     abstract class AObject
     {
+        public Color color; //Цвет команды
         public PointF position; //Позиция на карте
         public PointF target; //Цель
         public float vector; //Угол повората
