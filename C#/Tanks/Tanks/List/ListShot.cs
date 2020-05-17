75a35fcfe60cabeb3ae7e1a6cc2008d295c5e8bc
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-17 11:21:28 +0300

    Create ListShot.cs

---
+++ C#/Tanks/Tanks/List/ListShot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class ListShot
+    {
+        public List<Shot> listShot = new List<Shot>();
+
+        //Новый выстрел
+        public void NewShot(dynamic unit)
+        {
+            listShot.Add(new Shot()
+            {
+                color = unit.color,
+                position = unit.position,
+                target = unit.target,
+                vector = (float)Math.Atan2(unit.target.Y - unit.position.Y, unit.target.X - unit.position.X)
+            });
+        }
+        //Отрисовываем список снарядов
+        public void DrawListShot(Graphics g)
+        {
+            foreach (Shot shot in listShot)
+            {
+                shot.DrawShot(g);
+            }
+        }
+    }
+}
