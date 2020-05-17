4fe15311beebb18c115de988ca8b8f936f85e2f0
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-17 11:21:26 +0300

    Update FormTanks.Designer.cs

--- C#/Tanks/Tanks/FormTanks.Designer.cs
+++ C#/Tanks/Tanks/FormTanks.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Green;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1280, 720);
             this.Name = "FormTanks";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
