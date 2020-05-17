436be8848c0477cac4931c2c9403135318405b20
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-17 11:21:18 +0300

    Create Car.cs

---
+++ C#/Tanks/Tanks/Object/Car.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Car : Unit, IDrawn
+    {
+        private static Size size = new Size(64, 64);
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.Машинка);
+        private readonly Rectangle body = new Rectangle(new Point(0, 0), size);
+        Pen pen;
+
+        /// <summary>
+        /// Конструктор машинки
+        /// </summary>
+        public Car()
+        {
+            speed = 2;
+            life = 10;
+        }
+
+        //Отрисовка машинки
+        public void DrawUnit(Graphics g, Point cursor)
+        {
+            target = cursor;
+            PositionUnit();
+            vector = Vector(vector, speed);
+
+            //Машинка
+            g.TranslateTransform(position.X, position.Y);
+            g.RotateTransform(vector);
+            g.DrawImage(bitmap, -30, -27, body, GraphicsUnit.Pixel);
+            g.ResetTransform();
+
+            //Цвет команды
+            pen = new Pen(color, 3);
+            g.DrawEllipse(pen, position.X - 7, position.Y, 10, 10);
+
+            DrawInfo(g);
+        }
+    }
+}
