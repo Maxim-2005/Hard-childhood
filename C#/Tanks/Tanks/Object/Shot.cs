7f0177054b77e6d57843bbc52d4a6e1b1acf46f2
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-17 11:21:10 +0300

    Create Shot.cs

---
+++ C#/Tanks/Tanks/Object/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Shot : AObject
+    {
+        private PointF position0;
+        private Pen pen;
+
+        /// <summary>
+        /// Конструктор снаряда
+        /// </summary>
+        public Shot()
+        {
+            speed = 16;
+        }
+
+        //Отрисовка пульки
+        public void DrawShot(Graphics g)
+        {
+            position0 = position;
+            position = Position();
+            pen = new Pen(color, 3);
+            g.DrawLine(pen, position, position0);
+        }
+    }
+}
