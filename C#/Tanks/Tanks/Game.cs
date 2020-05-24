083b887a04936d64d2c6f06cffb2f735af1bb1be
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-24 11:11:07 +0300

    Update Game.cs

--- C#/Tanks/Tanks/Game.cs
+++ C#/Tanks/Tanks/Game.cs
@@ -1,117 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Threading.Tasks;
 
 namespace Tanks
 {
     class Game
     {
+        private List<ListUnit> ListParty;
         private ListUnit RedParty, BlueParty;
         private ListShot listShot;
 
         //Старт игры
         public void StartGame()
         {
-            RedParty = new ListUnit();
-            RedParty.CreateListUnit(Color.Red, 20);
+            ListParty = new List<ListUnit>();
 
-            BlueParty = new ListUnit();
-            BlueParty.CreateListUnit(Color.Blue, 80);
+            ListParty.Add(new ListUnit(Color.Red, 20));
+            ListParty.Add(new ListUnit(Color.Blue, 80));
+            ListParty.Add(new ListUnit(Color.Yellow, 60));
 
             listShot = new ListShot();
 
-            //SW();
-            Sound();
+            Sound.Music();
         }
 
         //Шаг игры
         public void StepGame(Graphics g, Point cursor)
         {
-            RedParty.DrawListUnit(g, cursor, listShot);
-            BlueParty.DrawListUnit(g, cursor, listShot);
+            foreach (ListUnit party in ListParty)
+                party.DrawListUnit(g, cursor, listShot);
 
             listShot.DrawListShot(g);
         }
-
-        #region//Звук заставки
-        private void SW()
-        //261 - до 1
-        //293 - ре 2
-        //329 - ми 3
-        //349 - фа 4
-        //392 - соль 5
-        //440 - ля 6
-        //493 - си 7
-        {
-            Console.Beep(440, 500);
-            Console.Beep(440, 500);
-            Console.Beep(440, 500);
-            Console.Beep(349, 350);
-            Console.Beep(523, 150);
-            Console.Beep(440, 500);
-            Console.Beep(349, 350);
-            Console.Beep(523, 150);
-            Console.Beep(440, 1000);
-            Console.Beep(659, 500);
-            Console.Beep(659, 500);
-            Console.Beep(659, 500);
-            Console.Beep(698, 350);
-            Console.Beep(523, 150);
-            Console.Beep(415, 500);
-            Console.Beep(349, 350);
-            Console.Beep(523, 150);
-            Console.Beep(440, 1000);
-        }
-
-        //Звук заставки
-        async private void Sound()
-        {
-            await Task.Run(() =>
-            {
-                Console.Beep(349, 250);
-                Console.Beep(330, 250);
-                Console.Beep(293, 250);
-                Console.Beep(261, 250);
-                Console.Beep(392, 500);
-                Console.Beep(392, 350);
-                Console.Beep(349, 250);
-                Console.Beep(329, 250);
-                Console.Beep(293, 250);
-                Console.Beep(261, 250);
-                Console.Beep(392, 500);
-                Console.Beep(392, 350);
-
-                Console.Beep(349, 250);
-                Console.Beep(440, 250);
-                Console.Beep(440, 250);
-                Console.Beep(349, 250);
-                Console.Beep(329, 250);
-                Console.Beep(392, 250);
-                Console.Beep(392, 250);
-                Console.Beep(329, 250);
-                Console.Beep(293, 250);
-                Console.Beep(329, 250);
-                Console.Beep(349, 250);
-                Console.Beep(293, 250);
-                Console.Beep(261, 500);
-                Console.Beep(261, 500);
-
-                Console.Beep(349, 250);
-                Console.Beep(440, 250);
-                Console.Beep(440, 250);
-                Console.Beep(349, 250);
-                Console.Beep(329, 250);
-                Console.Beep(392, 250);
-                Console.Beep(392, 250);
-                Console.Beep(329, 250);
-                Console.Beep(293, 250);
-                Console.Beep(329, 250);
-                Console.Beep(349, 250);
-                Console.Beep(293, 250);
-                Console.Beep(261, 500);
-                Console.Beep(261, 500);
-            });
-        }
-        #endregion
     }
 }
