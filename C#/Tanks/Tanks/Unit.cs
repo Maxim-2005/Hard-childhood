5172db106c6748b5c21c3604b6fb388e066f13e2
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-05-28 20:43:43 +0300

    Update Unit.cs

--- C#/Tanks/Tanks/Unit.cs
+++ C#/Tanks/Tanks/Unit.cs
@@ -9,7 +9,7 @@ namespace Tanks
         private static uint ID;
 
         public uint id = ++ID; //Номер танка
-        public float life; //Хит поинты
+        public float life; //Хитпоинты
         private Font font = new Font("Arial", 10, FontStyle.Bold, GraphicsUnit.Point);
         private SolidBrush color = new SolidBrush(Color.Yellow);
         private Pen pen = new Pen(Color.Red, 3);
