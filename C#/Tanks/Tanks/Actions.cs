e2f349f1995854dfa007e8721dcbf731e6c8d963
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-11 21:24:20 +0300

    Create Actions.cs

---
+++ C#/Tanks/Tanks/Actions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Actions
+    {
+        public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach(ListUnit party in ListParty)
+                foreach(dynamic unit in party.listUnits)
+                {
+                    unit.PositionUnit();
+                    unit.vector = unit.Vector(unit.vector, unit.speed);
+                }
+        }
+    }
+}
