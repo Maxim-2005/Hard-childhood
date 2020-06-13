639322edd7e2d1bbe39e36f83363098f2f519005
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-13 21:06:53 +0300

    Update Actions.cs

--- C#/Tanks/Tanks/Actions.cs
+++ C#/Tanks/Tanks/Actions.cs
@@ -4,9 +4,13 @@ namespace Tanks
 {
     class Actions
     {
+        private List<ListUnit> ListPartyl;
+
         //Перебор всех юнитов
         public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
         {
+            this.ListParty = ListParty;
+
             foreach (ListUnit party in ListParty)
                 foreach (dynamic unit in party.listUnits)
                     if (unit.act != Act.DEAD) Logic(unit);
@@ -16,21 +20,23 @@ namespace Tanks
         private void Logic(dynamic unit)
         {
             {
-                unit.PositionUnit();
-                unit.vector = unit.Vector(unit.vector, unit.speed);
-
                 switch (unit.act)
                 {
                     case Act.WAIT:
+                        goto case Act.FIRE;
+                        //ActWait(unit);
                         break;
 
                     case Act.FIND:
+                        ActFind(unit);
                         break;
 
                     case Act.MOVE:
+                        ActMove(unit);
                         break;
 
                     case Act.FIRE:
+                        ActFire(unit);
                         break;
 
                     default:
@@ -38,6 +44,32 @@ namespace Tanks
                         break;
                 }
             }
+        }
+
+        //Процесс ожидания
+        private void ActWait(dynamic unit)
+        {
+            if (unit.life <= 0)
+                unit.act = Act.DEAD;
+        }
+
+        //Процесс поиска
+        private void ActFind(dynamic unit)
+        {
+
+        }
+         
+        //Процесс перемещения
+        private void ActMove(dynamic unit)
+        {
+
+        }
+
+        //Процесс атаки
+        private void ActFire(dynamic unit)
+        {
+            unit.PositionUnit();
+            unit.vector = unit.Vector(unit.vector, unit.speed);
         }
     }
 }
