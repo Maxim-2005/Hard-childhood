66c762b5dd15b2a9fa27faaa85d08a1212eedf35
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-25 20:10:33 +0300

    Create Shooting.cs

---
+++ C#/Tanks/Tanks/Script/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tanks
+{
+    class Shooting
+    {
+        //Расчет стрельбы
+        public void ActShot(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach (Shot shot in listShot.listShot)
+            {
+                shot.MoveShot();
+
+                /*if (shot.speed < 2)
+                    listShot.RemoveShot(shot);*/
+            }
+        }
+    }
+}
