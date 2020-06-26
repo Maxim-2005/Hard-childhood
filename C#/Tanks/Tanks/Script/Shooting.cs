d725e8783e4587ca020339bde7679ea5619acd49
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-26 17:00:42 +0300

    Update Shooting.cs

--- C#/Tanks/Tanks/Script/Shooting.cs
+++ C#/Tanks/Tanks/Script/Shooting.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Tanks
 {
     class Shooting
     {
+        private ListShot listShot;
+
         //Расчет стрельбы
-        public void ActShot(List<ListUnit> ListParty, ListShot listShot)
+        async public void ActShot(List<ListUnit> ListParty, ListShot listShot)
         {
+            this.listShot = listShot;
+
             foreach (Shot shot in listShot.listShot)
             {
+                await Task.Run(() => Console.Beep(100, 100));
                 shot.MoveShot();
 
-                /*if (shot.speed < 2)
-                    listShot.RemoveShot(shot);*/
+                if (shot.speed < 5)
+                    listShot.RemoveShot(shot);
             }
         }
     }
