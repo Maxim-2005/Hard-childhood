34b6bb984ba0104278216f68a9e8ab5dea5dd7ac
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-26 17:00:37 +0300

    Create Bang.cs

---
+++ C#/Tanks/Tanks/Object/Bang.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace Tanks
+{
+    class Bang
+    {
+        public PointF position;
+        public byte time;
+
+        /// <summary>
+        /// Конструктор взрыва
+        /// </summary>
+        public Bang(PointF position)
+        {
+            this.position = position;   
+        }
+
+        public void DrawBang(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(Color.FromArgb(192 - time, time + 128, time, 0)),
+                new RectangleF(-time/2, -time/2, time, time));
+            g.ResetTransform();
+        }
+    }
+}
