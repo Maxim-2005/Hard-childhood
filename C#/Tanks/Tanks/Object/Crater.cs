a88f29bbfc27c4fbea5f07fa3bf9de27eb4be87f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-28 13:56:57 +0300

    Create Crater.cs

---
+++ C#/Tanks/Tanks/Object/Crater.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Crater
+    {
+        public PointF position;
+        public byte time;
+
+        /// <summary>
+        /// Конструктор воронки
+        /// </summary>
+        public Crater(PointF position)
+        {
+            this.position = position;
+        }
+
+        public void DrawCrater(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(Color.FromArgb(126, 0, 0, 0)),
+                new RectangleF(-time / 2, -time / 2, time, time));
+            g.ResetTransform();
+        }
+    }
+}
