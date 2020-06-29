55c29af8c606386a7b4bce01742b3bcaccf9668c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-06-29 21:30:37 +0300

    Create AObject.cs

---
+++ C#/Tanks/Tanks/Structure/AObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tanks
+{
+    abstract class AObject
+    {
+        public Color color; //Цвет команды
+        public PointF position; //Позиция на карте
+        public PointF target; //Цель
+        public float vector; //Угол повората
+        public float speed; //Скорость
+
+        //Расчет позиции
+        public PointF Position()
+        {
+            position.X += speed * (float)Math.Cos(vector);
+            position.Y += speed * (float)Math.Sin(vector);
+
+            return position;
+        }
+
+        ///<summary>
+        ///Расчет расстояния между точками (float)
+        ///<summary>
+        public float Delta(PointF point1, PointF point2)
+        {
+            float CathetX = point1.X - point2.X;
+            float CathetY = point1.Y - point2.Y;
+
+            return (float)Math.Sqrt(CathetX * CathetX + CathetY * CathetY);
+        }
+    }
+}
