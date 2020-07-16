e7fbddf088239c853f71b25fc8a3b447d9839bfb
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:04:00 +0300

    Create Fishes.cs

---
+++ C#/Fish/Fishes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Fish
+{
+    class Fishes
+    {
+        private Random random = new Random();
+        public List<object> ListFish = new List<object>();
+
+        public Fishes()
+        {
+            for (byte i = 0; i < 5; i++)
+            {
+                NewFish(new FishOne());
+                NewFish(new FishTwo());
+                NewFish(new FishThree());
+            }
+        }
+
+        private void NewFish(dynamic fish)
+        {
+            ListFish.Add(fish);
+            fish.position = Position();
+            fish.target = Position();
+        }
+
+        //Расчет позиции
+        public PointF Position()
+        {
+            PointF position = new PointF();
+            position.X = random.Next(20, 1900);
+            position.Y = random.Next(20, 1060);
+            return position;
+        }
+
+        public void DrawListFish(Graphics g)
+        {
+            foreach (dynamic fish in ListFish)
+            {
+                //fish.Position();
+                fish.Target();
+                fish.DrawFish(g);
+            }
+        }
+    }
+}
