9f23c3f28d5c246d63d531ddb988b43ae47d071f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:03:58 +0300

    Create FishTwo.cs

---
+++ C#/Fish/fish/FishTwo.cs
@@ -0,0 +1,22 @@
+﻿using Fish.fish;
+using System.Drawing;
+
+namespace Fish
+{
+    class FishTwo : AFish, IFish
+    {
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.latest1);
+
+        public FishTwo()
+        {
+            speed = 2;
+        }
+
+        //Отрисовка рыбки
+        public void DrawFish(Graphics g)
+        {
+            Position(bitmap);
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
