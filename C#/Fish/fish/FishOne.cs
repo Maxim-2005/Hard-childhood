f6ef0f7708e6033b66c79dea97933db175c24a2d
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:03:53 +0300

    Create FishOne.cs

---
+++ C#/Fish/fish/FishOne.cs
@@ -0,0 +1,22 @@
+﻿using Fish.fish;
+using System.Drawing;
+
+namespace Fish
+{
+    class FishOne : AFish, IFish
+    {
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.latest);
+
+        public FishOne()
+        {
+            speed = 1;
+        }
+
+        //Отрисовка рыбки
+        public void DrawFish(Graphics g)
+        {
+            Position(bitmap);
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
