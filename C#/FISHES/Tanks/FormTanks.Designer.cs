f08644ae2c5af915d40386da43da43d8ac983e70
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:04:50 +0300

    Create FormTanks.Designer.cs

---
+++ C#/FISHES/Tanks/FormTanks.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Tanks
+{
+    partial class FormTanks
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.RESTART = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 16;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // RESTART
+            // 
+            this.RESTART.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RESTART.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.RESTART.Location = new System.Drawing.Point(12, 685);
+            this.RESTART.Name = "RESTART";
+            this.RESTART.Size = new System.Drawing.Size(75, 23);
+            this.RESTART.TabIndex = 0;
+            this.RESTART.Text = "RESTART";
+            this.RESTART.UseVisualStyleBackColor = false;
+            this.RESTART.Click += new System.EventHandler(this.RESTART_Click);
+            // 
+            // FormTanks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.RESTART);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormTanks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tanks";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormTanks_Load);
+            this.Click += new System.EventHandler(this.FormTanks_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormTanks_Paint);
+            this.DoubleClick += new System.EventHandler(this.FormTanks_DoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button RESTART;
+    }
+}
+
