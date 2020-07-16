b2e511589f42ef4853b31af257127d3c14f1a35e
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:04:41 +0300

    Create ListShot.cs

---
+++ C#/FISHES/Tanks/List/ListShot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class ListShot
+    {
+        public List<Shot> listShot = new List<Shot>();
+        public List<Bang> listBang = new List<Bang>();
+        public List<Crater> listCrater = new List<Crater>();
+
+        //Новый выстрел
+        public void NewShot(dynamic unit)
+        {
+            listShot.Add(new Shot(unit));
+        }
+
+        //Удаление выстрела
+        public void RemoveShot(Shot shot)
+        {
+            listBang.Add(new Bang(shot.position));
+            listShot.Remove(shot);
+        }
+
+        //Удаление взрыва
+        public void RemoveBang(Bang bang)
+        {
+            listCrater.Add(new Crater(bang.position));
+            listBang.Remove(bang);
+        }
+
+        //Удаление кратора
+        public void RemoveCrater(Crater crater)
+        {
+            listCrater.Remove(crater);
+        }
+
+        //Отрисовываем список снарядов
+        public void DrawListCrater(Graphics g)
+        {
+            foreach (Crater crater in listCrater)
+                crater.DrawCrater(g);
+        }
+
+        //Отрисовываем список снарядов
+        public void DrawListShot(Graphics g)
+        {
+            foreach (Shot shot in listShot)
+                shot.DrawShot(g);
+            foreach (Bang bang in listBang)
+                bang.DrawBang(g);
+        }
+    }
+}
