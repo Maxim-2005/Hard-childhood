b643c5f979f605d5fc20aa4d83ce663bd9a07372
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-07-16 21:03:55 +0300

    Create FishThree.cs

---
+++ C#/Fish/fish/FishThree.cs
@@ -0,0 +1,22 @@
+﻿using Fish.fish;
+using System.Drawing;
+
+namespace Fish
+{
+    class FishThree : AFish, IFish
+    {
+        private readonly Bitmap bitmap = new Bitmap(Properties.Resources.latest2);
+
+        public FishThree()
+        {
+            speed = 3;
+        }
+
+        //Отрисовка рыбки
+        public void DrawFish(Graphics g)
+        {
+            Position(bitmap);
+            g.DrawImage(bitmap, position);
+        }
+    }
+}
