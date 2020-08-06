c805f68aeca6ca06c0ccfb845d1df26a1c2373fb
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-08-06 21:25:26 +0300

    Update Fishes.cs

--- C#/Fish/Fishes.cs
+++ C#/Fish/Fishes.cs
@@ -24,6 +24,12 @@ namespace Fish
             ListFish.Add(fish);
             fish.position = Position();
             fish.target = Position();
+
+            fish.bitmap.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+            if (fish.position.X > fish.target.X)
+            {
+                fish.bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
         }
 
         //Расчет позиции
@@ -32,15 +38,15 @@ namespace Fish
             PointF position = new PointF();
             position.X = random.Next(20, 1900);
             position.Y = random.Next(20, 1060);
+
             return position;
         }
-
+        
         public void DrawListFish(Graphics g)
         {
             foreach (dynamic fish in ListFish)
             {
-                //fish.Position();
-                fish.Target();
+                fish.Position();
                 fish.DrawFish(g);
             }
         }
