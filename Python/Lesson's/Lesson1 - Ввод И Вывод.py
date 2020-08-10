91e64612915604cb319b0fac275fa6e6d85c2432
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-08-10 20:10:31 +0300

    Create Lesson1 - Ввод И Вывод.py

---
+++ Python/Lesson's/Lesson1 - Ввод И Вывод.py
@@ -0,0 +1,32 @@
+# Урок номер №1
+# Вывод текста
+print("Hello World!\n" + '\t My Name Maxim')
+# Вывод вычислений
+print(1+2+3+4+5)
+
+# Создание переменной
+x = 20 # -20
+# Вывод переменной
+print(x)
+# Если, то, иначе
+if x > 0:
+    print("Число = " + str(x))
+# Смена значения переменной
+x = "Maxim"
+#Вывод текста с переменной
+print("Число = " + x)
+
+x = input("Как тебя зовут?\n")
+print("Очень приятно,", x)
+print("")
+
+# Ввод
+input()
+
+# Запуск с консоли
+# D:\Documents\MyPrograms\25.Python>"C:\Program Files (x86)\Python38-32/python.exe" Lesson1.py
+
+# Запуск с патчем
+# D:\Documents\MyPrograms\25.Python>Lesson1.py
+
+# Установка компилятора с консоли: pip install pyinstaller
