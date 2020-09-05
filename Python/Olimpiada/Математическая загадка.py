c5b80508a26bc4ef2bb6edec177844ae527efc9f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-05 20:02:59 +0300

    Create Математическая загадка.py

---
+++ Python/Olimpiada/Математическая загадка.py
@@ -0,0 +1,17 @@
+x = int(input())
+c = 0
+kur = []
+if x >= 11 or x <= 0:
+	print("-1")
+else:
+	while c < x:
+		m = input()
+		kur.append(m)
+		c += 1
+	for i in kur:
+		k = int(i)
+		if k <= 0 or k >= 1001:
+			print("-1")
+		else:
+			k = (k-100)//7
+			print(k)
