19efcc49139829eb16ad458e12cfb52b6fda0db4
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-13 17:06:04 +0300

    Update Фотограф.py

--- Python/Olimpiada/Фотограф.py
+++ Python/Olimpiada/Фотограф.py
@@ -1,14 +1,21 @@
 # Фотограф
 
 #ВВОД
-N = input("Введите диапозон номеров")
-M = input("Сколько всего номеров")
-arr = input("Вводим массив")
+N = 10 #input("Введите диапозон номеров")
+M =  7 #input("Сколько всего номеров")
+arr = "1 3 6 4 3 2 9" #input("Вводим массив")
+arr = arr.split()
+setArr = set(arr)
 
 #РЕШЕНИЕ
-maxN = 0
 sumN = 0
-
+for i in setArr:
+	temp = arr.count(i)
+	if temp > sumN:
+		maxN = i
+		sumN = temp
+	elif temp == sumN and int(maxN) > int(i):
+		maxN = i
 #ВЫВОД
 print(maxN, sumN)
 
