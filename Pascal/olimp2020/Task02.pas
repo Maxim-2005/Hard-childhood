2591ddc44e5855775efa75a890e41aaabee3aa97
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-20 21:13:00 +0300

    Create Task02.pas

---
+++ Pascal/olimp2020/Task02.pas
@@ -0,0 +1,12 @@
+﻿program Task2;
+
+var
+  S, A, B, C: integer;
+
+begin
+  readln(A, B, C);
+  if (A < B) and (A < C) then S := B + C
+  else if (C < A) and (C < B) then S := A + B
+  else S := A + C;
+  write(S);
+end.
