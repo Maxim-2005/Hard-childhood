5df50fe00be5ad603ee408d77026e938182f4e6e
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-20 21:12:51 +0300

    Create Olimpiada2.pas

---
+++ Pascal/olimp2019/Olimpiada2.pas
@@ -0,0 +1,16 @@
+﻿program Olimpiada2;
+
+var a, b, c, s:integer;
+
+begin
+  
+  Writeln ('Введите 3 числа');
+  Readln (a, b, c);
+  
+  if (a < b) and (a < c) then s := b + c
+  else if (b < a) and (b < c) then s := a + c
+  else s := a + b;
+  
+  Write ('Сумма наибольших значений: ', s);
+  
+end.
