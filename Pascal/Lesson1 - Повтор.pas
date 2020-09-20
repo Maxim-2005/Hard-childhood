11532fce78857ad865a5a33840ef059bbe64b8a7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-20 21:12:48 +0300

    Create Lesson1 - Повтор.pas

---
+++ Pascal/Lesson1 - Повтор.pas
@@ -0,0 +1,17 @@
+﻿program lesson1;
+
+var
+  x: integer;
+
+begin
+  write('Введите число: ');
+  read(x);
+  if (x < 100) and (x > 0)
+    then writeln('X больше 100')
+  else write('X меньше 100');
+  while (x > 0) do
+  begin
+    x := x-1;
+    writeln(x);
+  end;
+end.
