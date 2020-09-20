1e07c47f876ceb53a207cf30e1f784f7add95cdb
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-20 21:12:58 +0300

    Create Task01.pas

---
+++ Pascal/olimp2020/Task01.pas
@@ -0,0 +1,11 @@
+﻿program Task1;
+var  N: integer;
+begin
+  read(N);
+  if (N < 100) or (N > 999) then
+    writeln('FALSE')
+  else
+    if ((N div 100) + (N div 10 mod 10) + (N mod 10)) = 13 then
+     writeln('ENTER')
+    else writeln('LOCK')
+end.
