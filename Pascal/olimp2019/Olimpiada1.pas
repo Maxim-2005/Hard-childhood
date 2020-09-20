aa22bdd5c878f91f9819c7ab4d33e2a26454839c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-20 21:12:49 +0300

    Create Olimpiada1.pas

---
+++ Pascal/olimp2019/Olimpiada1.pas
@@ -0,0 +1,12 @@
+﻿program Olimpiada1;
+var N:word;
+begin
+write('Введите число от 1 до 10000: ');
+read(N);
+if (N<100) or (N>999)
+then writeln('FALSE')
+else
+  if N div 100 + (N mod 100) div 10 + N mod 10 = 13
+  then writeln('ENTER')
+  else writeln('LOCK');
+end.
