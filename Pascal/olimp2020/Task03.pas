5f96cdd7a3104bad89de04fb0d67e080bc4664fc
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-21 20:35:50 +0300

    Update Task03.pas

---
+++ Pascal/olimp2020/Task03.pas
@@ -0,0 +1,15 @@
+﻿program Task3;
+
+var
+  Y, X, S, A, B, C, D: integer;
+
+begin
+  read(A, B, C, D);
+  S := A + B + C + D;
+  X := S mod 10;
+  Y := S div 10 mod 10;
+  if (X = 1) and (Y <> 1) then writeln(S, ' птица')
+  else
+    if ((X = 2) or (X = 3) or (X = 4)) and (Y <> 1) then writeln(S, ' птицы')
+  else writeln(S, ' птиц')
+end.
