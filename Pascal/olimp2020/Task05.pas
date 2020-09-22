26c14de0da313b0c5f1fe5912f24940eebe1c497
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-22 21:03:21 +0300

    Create Task05.pas

---
+++ Pascal/olimp2020/Task05.pas
@@ -0,0 +1,15 @@
+﻿program Task5;
+
+var
+  arr: array[1..20] of integer;
+  N, S: integer;
+
+begin
+  read(N);
+  for var i := 1 to N do
+  begin
+    read(arr[i]);
+    if i < arr[i] then S := S+arr[i];
+  end;
+  write(S);
+end.
