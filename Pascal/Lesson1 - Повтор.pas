698c1cd370e9f04ad5b90385fdb13261c5a27996
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-09-27 20:30:09 +0300

    Update Lesson1 - Повтор.pas

--- Pascal/Lesson1 - Повтор.pas
+++ Pascal/Lesson1 - Повтор.pas
@@ -2,16 +2,29 @@
 
 var
   x: integer;
+  StatArr: array[0..5] of integer;
+  DynArr: array of integer;
 
 begin
   write('Введите число: ');
   read(x);
-  if (x < 100) and (x > 0)
+  if (x < 10) and (x > 0)
     then writeln('X больше 100')
   else write('X меньше 100');
   while (x > 0) do
   begin
-    x := x-1;
+    x := x - 1;
     writeln(x);
   end;
+  
+  StatArr[0] := 111;
+  writeln(StatArr[0]);
+  writeln(StatArr);
+  
+  setlength(DynArr, 10);
+  writeln(DynArr);
+  
+  for var i := 0 to High(DynArr) do
+    DynArr[i] := i*i;
+  writeln(DynArr);
 end.
