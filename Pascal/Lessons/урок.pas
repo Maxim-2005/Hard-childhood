21afb22761230e31428044b3a8754fd99e7a27a0
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:42 +0300

    Create урок.pas

---
+++ Pascal/Lessons/урок.pas
@@ -0,0 +1,39 @@
+﻿Program P2;
+
+Var A, B, I:Byte;
+Rand :word;
+
+Begin
+  
+randomize;
+Rand :=random(1,100);
+Writeln (Rand);
+
+case Rand of
+1: Write ('Ето раз');
+2..50: Write ('Много');
+51..100: Write ('Сверх много');
+else Write ('Шанс 1/100');
+end;
+
+A :=10;
+B :=5;
+I :=0;
+
+for var G :=0 to 20 do
+  Write ('*');
+  writeln;
+if A>B then 
+  begin  
+  while I < 8 do
+    begin
+      Writeln (A+b);
+      i := I+1;
+    end
+  end
+else
+  repeat 
+    Write (A*B);
+  until I<4;
+
+End.
