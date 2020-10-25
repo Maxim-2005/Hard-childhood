2bce48423ed617d655e68fa52cc72c17160b07db
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:23 +0300

    Create VM_5.pas

---
+++ Pascal/Lessons/VM_5.pas
@@ -0,0 +1,13 @@
+﻿Program Task5;
+var M,B,N:integer;
+begin
+  M:=0;
+read(N);
+ while N > 0 do
+   begin
+        B := N mod 10;
+        N := N div 10;
+        M := M*10 + B;
+    end;
+Write(M);
+end.
