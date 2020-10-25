d898c6731a00a29c2797b5e72c2ab7926fde71b7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:25 +0300

    Create Больше меньше ч.pas

---
+++ Pascal/Lessons/Больше меньше ч.pas
@@ -0,0 +1,24 @@
+﻿program bm;
+
+var RND, ST :word;
+
+begin
+
+//Create A Random Number
+randomize;
+RND :=random(1,1000);
+
+repeat
+
+//Player Choice
+Write ('Введите число: ');
+Read (ST);
+
+//Check > < =
+if RND>ST then Writeln ('Больше');
+if RND<ST then Writeln ('Меньше');
+if RND=ST then Writeln ('Вы угадали: ', RND);
+
+until RND=ST;
+
+end.
