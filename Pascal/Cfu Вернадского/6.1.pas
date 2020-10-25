6c807d88ff7927a0d20f70626539404b9aef54c5
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:11 +0300

    Create 6.1.pas

---
+++ Pascal/Cfu Вернадского/6.1.pas
@@ -0,0 +1,15 @@
+﻿program Task;
+var A1, B1, A2, B2, z1, z2, h :integer;
+begin
+read(A1, B1, A2, B2);
+ 
+if A1 > B1 then z1 := A1
+else z1 := B1;
+
+if A2 > B2 then z2 := A2
+else z2 := B2;
+
+h := z1+z2;
+
+write(h);
+end.
