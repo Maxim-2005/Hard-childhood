986f57d2e8166afbf7d8c85cc92dd4cff489337f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:17 +0300

    Create Scearch number.pas

---
+++ Pascal/Lessons/Scearch number.pas
@@ -0,0 +1,15 @@
+﻿program Num50;
+var s, p, n, n1, n2, n3 :integer;
+begin
+  n :=0;
+  While n<1000 do    
+    begin      
+      n :=n+1;
+      n1 :=n mod 10;
+      n2 :=n div 100;
+      n3 :=(n mod 100) div 10;
+      s :=n1+n2+n3;
+      p :=n1*n2*n3;
+      if s+50 = p then writeln (n);
+    end;
+end.
