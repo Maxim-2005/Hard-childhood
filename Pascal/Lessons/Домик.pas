67317f960f51a19f7d5809d1a0eae94f68789121
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-25 20:38:36 +0300

    Create Домик.pas

---
+++ Pascal/Lessons/Домик.pas
@@ -0,0 +1,26 @@
+﻿program P7;
+
+uses GraphABC;
+
+begin
+  
+  Pen.Color := RGB (170, 150, 0);
+  
+  //Солнце
+  DrawCircle (200, 50, 30);
+  
+  //Крыша
+  line (100, 100, 150, 50);
+  line (200, 100, 150, 50);
+  
+  Pen.Color := RGB (150, 150, 150);
+  
+  //Стены
+  DrawRectangle (200, 200, 100, 100);
+  
+  Pen.Color := RGB (150, 220, 240);
+  
+  //Окно
+  DrawCircle (150, 150, 25);
+  
+end.
