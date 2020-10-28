824d13fa3bdbc6308176cfa7bc0494f5541198a1
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-28 21:12:49 +0300

    Create 6.3.pas

---
+++ Pascal/Cfu Вернадского/6.3.pas
@@ -0,0 +1,22 @@
+﻿program task3;
+
+  var n, i, h0, h1, h2, h3 :int64;
+    arr: array of int64;
+
+begin
+  h1 := 1; h2 := 1; h3 := 1;
+  read(n);
+  if n < 4 then
+    write(1)
+  else
+    begin
+      for i := 4 to n do
+      begin
+        h0 := h1;
+        h1 := h2;
+        h2 := h3;
+        h3 := h0 + h1 + h2;
+      end;
+      write(h3);
+    end;
+end.
