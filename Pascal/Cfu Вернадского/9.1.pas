bf6379e566a1de3cecd5493e0e2d6767a3c97044
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-29 20:55:04 +0300

    Create 9.1.pas

---
+++ Pascal/Cfu Вернадского/9.1.pas
@@ -0,0 +1,13 @@
+﻿program task9;
+var N, i, m :integer;
+begin
+  read(N);
+  m := 0;
+  
+  for i := 1 to trunc(sqrt(N)) do
+  begin
+    if N mod i = 0 then
+      m := m + 1;
+  end;
+  write(m);
+end.
