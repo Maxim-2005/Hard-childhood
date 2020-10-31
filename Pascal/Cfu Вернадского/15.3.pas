9d254a5b045758d8a804771fac7410573d57ab7a
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-31 18:37:07 +0300

    Create 15.3.pas

---
+++ Pascal/Cfu Вернадского/15.3.pas
@@ -0,0 +1,29 @@
+﻿program Task;
+
+var
+  T, N, i, j, x: integer;
+  m: string;
+  arr: array of integer;
+
+begin
+  read(T);
+  setLength(arr, T);
+
+  for i := 0 to T - 1 do
+  readln(arr[i]);
+
+  for i := 0 to T - 1 do
+  begin
+    m := 'NO';
+    for j := 1 to trunc(sqrt(arr[i])) do
+    begin
+      x := arr[i] - j * j;
+      if (x > 0) and (frac(sqrt(x)) = 0) then
+      begin
+        m := 'YES';
+        break;
+      end;
+    end;
+    writeln(m);
+  end;
+end.
