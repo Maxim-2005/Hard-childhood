90bd55501e1c9d012d126ab194839ab64eed46aa
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-10-31 18:37:03 +0300

    Create 15.1.pas

---
+++ Pascal/Cfu Вернадского/15.1.pas
@@ -0,0 +1,15 @@
+﻿program Task;
+
+var
+  N, i, S, x: integer;
+
+begin
+  S := 0;
+  readln(N);
+  for i := 1 to N do
+  begin
+    readln(x);
+  if x > 0 then S := S + 1;
+  end;
+  writeln(S);
+end.
