136d6ec12dbdc3748af21b85885b9868166394fa
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-11-04 21:31:57 +0300

    Create 3.3.pas

---
+++ Pascal/Cfu Вернадского/3.3.pas
@@ -0,0 +1,15 @@
+﻿program taksPi;
+var K, i, x : integer;
+    V :double;
+begin
+read(K);
+x := 1;
+
+  for i := 0 to K-1 do
+  begin
+    V := V + 4/(i*2+1)*x;
+    x := x*(-1);
+  end;
+  
+  write(V:0:9)
+end.
