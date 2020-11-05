2f5a3b32dfd7612988d104f104a00a3f9aef11e9
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-11-05 20:59:00 +0300

    Create 11.2.pas

---
+++ Pascal/Cfu Вернадского/11.2.pas
@@ -0,0 +1,17 @@
+﻿program task;
+var N, i, x :integer;
+arr: array of integer;
+begin
+ read(N);
+ setLength(arr, N);
+ 
+ for i := 0 to N-1 do
+ begin
+   read(arr[i]);
+ end;
+ 
+ for i := 0 to N-1 do
+   if arr[i] = 0 then writeln(2)
+    else if arr[i] = 1 then writeln(0)
+      else writeln(1);
+end.
