9e8092b2fe35bd0aab4b6fb7d7237e562446a086
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-11-14 21:23:59 +0300

    Create 11.3.pas

---
+++ Pascal/Cfu Вернадского/11.3.pas
@@ -0,0 +1,26 @@
+﻿program Task;
+
+var
+  N, s1, s2, i, X, bigX: integer;
+
+begin
+  read(N);
+  x := 0;
+  s2 := 0;
+  
+  for i := 1 to N do
+  begin
+    read(s1);
+    if s1 >= s2 then 
+    begin
+      X := X + 1;
+      if bigX <= X then
+        bigX := X;
+    end
+    else
+      X := 1;
+  s2 := s1;
+  end;
+  
+  write(bigX);
+end.
