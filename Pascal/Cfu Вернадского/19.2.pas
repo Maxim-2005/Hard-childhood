ad8d155c60433e8ecafe1e702546158706377b7a
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-11-26 14:48:32 +0300

    Create 19.2.pas

---
+++ Pascal/Cfu Вернадского/19.2.pas
@@ -0,0 +1,15 @@
+﻿program task;
+var w1, w2:string;
+i :char;
+n : integer;
+begin
+  
+  readln(w1);
+  readln(w2);
+  for i := 'a' to 'z' do
+  begin
+  if (pos(i, w1) > 0) and (pos(i, w2) > 0) then
+  n := n+1;
+  end;
+  write(n);
+end.
