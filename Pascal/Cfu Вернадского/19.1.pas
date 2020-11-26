502be52b98d7c60844f99a49b15c7e4fec8f286e
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-11-26 14:48:35 +0300

    Create 19.1.pas

---
+++ Pascal/Cfu Вернадского/19.1.pas
@@ -0,0 +1,11 @@
+﻿program task;
+
+var N :integer;
+
+begin
+  read(N);
+  if N mod 4 = 0 then write(-N)
+  else if (N + 1) mod 4 = 0 then write(0)
+  else if (N + 2) mod 4 = 0 then write(N+1)
+  else write(1);
+end.
