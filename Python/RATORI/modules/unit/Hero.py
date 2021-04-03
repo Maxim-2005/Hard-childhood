c4502d95cedc28786950d2b4cf525896a0270e27
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-04-03 21:27:54 +0300

    image

--- Python/RATORI/modules/unit/Hero.py
+++ Python/RATORI/modules/unit/Hero.py
@@ -8,11 +8,25 @@ class Hero(object):
         """Конструктор"""
         self.rate = self._rate_
         self.rect = pg.Rect(0, 0, self.rate, self.rate)
+        self.tile_atlas = []
+        self.tile_atlas = self.filling()
 
     def draw(self, g):
         """Отрисовка"""
+        image = self.tile_atlas[4][7]
+        g.blit(image, self.rect)
+
+    # Отрисовка Трраина
+    def filling(self):
+        """Заполняем атлас тайлами"""
+        atlas = self._atlas_
         rate = self.rate
         size = (rate, rate)
-        rect = (0, 0)
-        image = self._atlas_.subsurface(rect, size)
-        g.blit(image, self.rect)
+        for row in range(atlas.get_height() // rate):
+            self.tile_atlas.append([])
+            for col in range(atlas.get_width() // rate):
+                rect = (col * rate, row * rate)
+                image = atlas.subsurface((rect, size))
+                image = pg.transform.scale(image, (rate*2, rate*2))
+                self.tile_atlas[row].append(image)
+        return self.tile_atlas
