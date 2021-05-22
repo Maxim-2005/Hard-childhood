ad3d93d5f99fe69dec22bfddaf8c989309f578a3
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-05-22 20:52:52 +0300

    hp

--- Python/RATORI/modules/interface/Interface.py
+++ Python/RATORI/modules/interface/Interface.py
@@ -1,3 +1,4 @@
+from modules.interface.Life import Life
 from modules.interface.MiniMap import MiniMap
 
 class Interface(object):
@@ -5,11 +6,14 @@ class Interface(object):
         """Конструктор"""
         self.size = size
         self.minimap = MiniMap(self.size)
+        self.life = Life()
 
     def update(self, hero):
         """Обновление"""
         self.minimap.update(hero)
+        self.life.update()
 
     def draw(self, g):
         """Отрисовка"""
         self.minimap.draw(g)
+        self.life.draw(g)
