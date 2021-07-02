406306412bf8ba073779f6f5032eb93f0e7d9554
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-07-02 20:09:53 +0300

    Cat final

--- Python/RATORI/modules/unit/Cat.py
+++ Python/RATORI/modules/unit/Cat.py
@@ -9,29 +9,28 @@ class Cat(object):
     def filling():
         """Заполняем атлас тайлами"""
         pg.init()
-        rate_x = 32
-        rate_y = 32
+        rate = 32 * 2
         atlas = pg.image.load('images\\cat.png')
-        atlas = pg.transform.scale(atlas, (12 * rate_x, 8 * rate_y))
+        atlas = pg.transform.scale(atlas, (9 * rate, 4 * rate))
         tile_atlas = []
-        for row in range(atlas.get_height() // rate_y):
+        for row in range(atlas.get_height() // rate):
             tile_atlas.append([])
-            for col in range(atlas.get_width() // rate_x):
-                rect = (rate_x * col, rate_y * row)
-                image = atlas.subsurface((rect, (rate_x, rate_y)))
+            for col in range(atlas.get_width() // rate):
+                rect = (rate * col, rate * row)
+                image = atlas.subsurface((rect, (rate, rate)))
                 tile_atlas[row].append(image)
         return tile_atlas
 
     def __init__(self, size, tile_atlas):
         """Конструктор"""
-        self.rate_x = 32
-        self.rate_y = 32
+        self.rate_x = 32 * 2
+        self.rate_y = 32 * 2
         self.step = 0
         self.row = 0
-        self.col = 0
-        self.time_move = 10
+        self.col = 8
+        self.time_move = 20
         self.scroll_line = 10
-        self.unit_turn = 0
+        self.unit_turn = 8
         self.scroll = self.scroll_line / 1.4
         self.tile_atlas = tile_atlas
         self.point_x = r(size[0] // 8, size[0] * 3 // 4)
@@ -44,10 +43,10 @@ class Cat(object):
         self.rect.x, self.rect.y = self.pos_unit(turn)
         if self.time_move < 1:
             self.unit_turn = r(0, 8)
-            self.time_move = r(10, 60)
+            self.time_move = r(30, 150)
         self.time_move -= 1
-        if self.unit_turn > 1:
-            self.image = self.tile_atlas[0][0]
+        if self.unit_turn > 7:
+            self.image = self.tile_atlas[0][8]
         else:
             self.col = self.unit_turn
             self.image = self.select()
@@ -58,8 +57,15 @@ class Cat(object):
 
     def select(self):
         """Выбор картинки шага"""
-        if self.col
-        self.col += 1
+        if self.step > 10:
+            if self.row >= 3:
+                self.row = 0
+            else:
+                self.row += 1
+                self.step = 0
+        else:
+            self.step += 5
+        return self.tile_atlas[self.row][self.col]
 
     def pos_unit(self, turn):
         """Позиция юнита"""
