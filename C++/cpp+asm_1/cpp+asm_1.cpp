0e16e097e4a3ec4f05dd15a724e36a7923dd06bb
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-12-18 18:31:38 +0300

    new

---
+++ C++/cpp+asm_1/cpp+asm_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    
+    int x;
+    int y;
+    int res;
+
+    cin >> x;
+    cin >> y;
+
+    __asm
+    {
+        mov eax, x
+        mov ebx, y
+        add eax, ebx
+        mov res, eax
+    }
+
+    // Повторное использование данных
+
+    cout << res;
+    cin >> x;
+    __asm
+    {
+        mov eax, x
+        //mov ebx, y
+        add eax, ebx
+        mov res, eax
+    }
+
+    cout << res;
+}
