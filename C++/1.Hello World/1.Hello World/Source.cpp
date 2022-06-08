66241f84de8f98341f033b16c43edea0fb0a1978
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-06-08 19:42:52 +0300

    бинарник

---
+++ C++/1.Hello World/1.Hello World/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //Стандартная библиотека ввода и вывода
+
+using namespace std; //Пространство имен для библиотеки
+
+void main () //Старт программы
+{
+	cout <<"Hello World"<< endl; //Вывод на экран консоли
+	system ("pause"); //Пауза
+}
---
+++ C++/Бинарные операции/Бинарные операции.cpp
@@ -0,0 +1,66 @@
+﻿// Бинарные операции.cpp
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int x = 0b0011; //3
+    int y = 0x5; //5
+    int z;
+
+    cout << "x = " << x <<  ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl;
+    cout << "Логическое не: " << endl;
+    x = ~x;
+    y = ~y;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl;
+    cout << "Логическое и: " << endl;
+    x = 3;
+    y = 5;
+    z = x & y;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z = " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl;
+    cout << "Логическое или: " << endl;
+    z = x | y;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z = " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl;
+    cout << "Логическое исключающее или: " << endl;
+    z = x ^ y;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z = " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl;
+    cout << "Логическое побитовый сдвиг в право: " << endl;
+    x = 3;
+    y = x >> 1;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl;
+    cout << "Логическое побитовый сдвиг в лево: " << endl;
+    x = 3;
+    y = x << 1;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y = " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    x &= y;
+    cout << "x = " << x << ", бинарно: " << bitset<8>(x) << endl;
+
+    system("pause > nul");
+    return 0;
+}
