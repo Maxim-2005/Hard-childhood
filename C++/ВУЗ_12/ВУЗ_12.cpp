999c8a6cbf337bd8d9f6d37df0d1f7bed5ecbb47
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-07-19 21:13:52 +0300

    вуз

---
+++ C++/ВУЗ_12/ВУЗ_12.cpp
@@ -0,0 +1,19 @@
+﻿// ВУЗ_12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int x, t = 1;
+
+    cin >> x;
+
+    for (int i = 1; i <= x; i++) {
+        t *= i;
+    }
+    cout << t;
+}
---
+++ Python/ВУЗ первый курс/ВУЗ_12.py
@@ -0,0 +1,6 @@
+x = int(input())
+t = 1
+
+for i in range(1, x + 1):
+	t *= i
+print(t)
