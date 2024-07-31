fa6998b0e6d829ecd0494a4d7e58ace43467b16b
Maxim <maxim2005ua@gmail.com>
2024-07-31 16:19:13 +0300

    Подстранички

--- Python/Host Maid/app.py
+++ Python/Host Maid/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return("Hello World!")
+    return(render_template("index.html"))
+
+@app.route('/about')
+def about():
+    return(render_template("about.html"))
 
 if __name__ == "__main__":
     app.run(debug=True)
